d061b41e11b408e78957ec6d7a5a9358efd3db9e
Josh Gearheart <other.josh.gearheart@gmail.com>
2025-01-28 12:06:22 -0800

    add pixelgrid generator

--- script.js
+++ script.js
@@ -3,8 +3,27 @@
 const expandBody = function (idString) {
   const bodyID = idString.split("-")[0] + "-body";
   $(bodyID).toggle();
-  $(idString).children().eq(0).children().eq(1).toggleClass("flip")
+  $(idString).children().eq(0).children().eq(1).toggleClass("flip");
   $(idString).toggleClass("roundtop rounded");
+};
+
+const createGrid = function (width, height) {
+  const pixelSize = 50 / Number(width);
+  let pixelGrid = "<div class='flex flexcolumn'>";
+  let row = '<div class="flex flexrow">';
+  for (let index = 0; index < width; index++) {
+    row += "<div class='pixel'></div>";
+  }
+  row += "</div>";
+  for (let index = 0; index < height; index++) {
+    pixelGrid += row;
+  }
+  pixelGrid += "</div>";
+  $('.pixel').css({
+    height: pixelSize + "vw",
+    width: pixelSize + "vw"
+  })
+  return pixelGrid;
 };
 
 // event listeners
